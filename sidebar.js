1775b4e520179a133231a63a441cc296a7c5f73a
Andrew Lee <leeandrew135@gmail.com>
2024-05-08 18:10:39 -0400

    desktop home page finished

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,7 @@ function showSidebar() {
   var sidebar = document.getElementById("navBar");
   var shade = document.getElementById("navBar2");
 
-  if (sidebar.style.left === "0px" || sidebar.style.left === "") {
+  if (sidebar.style.left === "0px") {
     sidebar.style.left = "-75vw"; // Move the sidebar off-screen
     shade.style.display = "none";
   } else {
